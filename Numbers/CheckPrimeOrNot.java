28cac4596a7ef84a05d286d0d964b8bdd84dc436
Abhijeet Tiwari <abhijeett709@gmail.com>
2026-03-16 12:23:38 +0530

    Check Prime number added to Number Folder

---
+++ Numbers/CheckPrimeOrNot.java
@@ -0,0 +1,36 @@
+package Numbers;
+import java.util.*;
+
+public class CheckPrimeOrNot {
+    public static boolean prime(int n)
+    {
+        if(n<2)
+        {
+            return false;
+        }
+        for(int i = 2 ; i*i<=n ; i++)
+        {
+            if((n%i)==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+        boolean check = prime(n);
+        if(check)
+        {
+            System.out.println("The number is prime number");
+        }
+        else
+        {
+            System.out.println("The number is not prime number");
+        }
+        sc.close();
+    }
+    
+}
